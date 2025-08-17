74f61e8f6d54ef9b5feffe893d640a5339350a70
Amirmohammad <amirmuhaammaadd@gmail.com>
2025-08-17 12:19:14 +0330

    saman-Input

---
+++ src/_components/app-input.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { useState } from "react";
+import { type ComponentProps } from "react";
+
+type AppInputProps = ComponentProps<"input"> & {
+    label?: string;
+    helpText?: string;
+    errorMessage?: string;
+    maxLength?: number;
+    hasStar?: boolean;
+};
+
+const AppInput = ({
+    maxLength,
+    className,
+    hasStar,
+    errorMessage,
+    label,
+    helpText,
+    onChange,
+    onBlur,
+    ...rest
+}: AppInputProps) => {
+    const [value, setValue] = useState("");
+    const [error, setError] = useState<string | null>(null);
+    const [touched, setTouched] = useState(false);
+
+    const validate = (val: string) => {
+        if (maxLength && val.length > maxLength) {
+            setError(errorMessage || `حداکثر ${maxLength} کاراکتر مجاز است`);
+        } else {
+            setError(null);
+        }
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const tempValue = e.target.value;
+        setValue(tempValue);
+        validate(tempValue);
+
+        if (onChange) onChange(e);
+    };
+
+    const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+        setTouched(true);
+        validate(value);
+
+        if (onBlur) onBlur(e);
+    };
+
+    return (
+        <div className="flex flex-col gap-1 w-[300px]">
+            {label && (
+                <label className="mb-1 text-gray-700">
+                    {hasStar && <span className="text-red-500 ml-2">*</span>}
+                    {label}
+                </label>
+            )}
+
+            <input
+                value={value}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                maxLength={maxLength ? maxLength + 1 : undefined}
+                {...rest}
+                className={`border px-3 w-full py-2 rounded-md text-sm outline-none transition-all 
+                     ${error ? "border-red-500 focus:ring-red-300" : "border-gray-300 focus:ring-blue-300"}
+                     ${className || "bg-white"}
+                 `}
+            />
+
+            {!error && helpText && (
+                <span className="text-xs text-gray-500">{helpText}</span>
+            )}
+
+            {(error || touched) && (
+                <span className="text-xs text-red-500">{error}</span>
+            )}
+
+            {/* -------------- Option one --------------- */}
+
+            {/* {error && (
+                <span className="text-xs text-red-500">{error}</span>
+            )} */}
+
+            {/* -------------- Option two --------------- */}
+
+            {/* {error && (
+                <span className="text-xs text-red-500">{errorMessage}</span>
+            )} */}
+        </div>
+    );
+};
+
+export default AppInput;
---
+++ src/app/(pages)/form/page.tsx
@@ -0,0 +1,84 @@
+"use client"
+
+import AppInput from "@/_components/app-input";
+import { useState } from "react";
+
+export type UserData = {
+    personOne: string,
+    personTwo: string,
+}
+
+const FormPage = () => {
+
+    const [userData, setUserData] = useState<UserData>({
+        personOne: "",
+        personTwo: "",
+    })
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (userData.personOne.length === 0 || userData.personOne.length > 10) {
+            alert("نام نفر اصلی معتبر نیست!");
+            return;
+        }
+
+        if (userData.personTwo.length === 0 || userData.personTwo.length > 5) {
+            alert("نام شخص ثالث معتبر نیست!");
+            return;
+        }
+
+        alert("اطلاعات ارسال شد");
+
+        setUserData({
+            personOne: "",
+            personTwo: ''
+        })
+    };
+
+
+    return (
+        <div className="flex items-center justify-center gap-6 h-screen flex-col">
+
+            <form
+                onSubmit={handleSubmit}
+                className="flex flex-col gap-8 max-w-sm mx-auto border p-10 border-slate-300 rounded-md bg-slate-50 shadow-xl"
+            >
+
+                <AppInput
+                    onChange={(e) => setUserData({
+                        ...userData,
+                        personOne: e.target.value
+                    })}
+                    value={userData.personOne}
+                    label="نام نفر اصلی"
+                    placeholder="نام خود را وارد کنید"
+                    hasStar
+                    maxLength={10}
+                    helpText="حداکثر 10 کاراکتر مجاز است"
+                    errorMessage="طول نام نفر اصلی بیش از حد مجاز است"
+                />
+
+                <AppInput
+                    onChange={(e) => setUserData({
+                        ...userData,
+                        personTwo: e.target.value
+                    })}
+                    label="نام شخص ثالث"
+                    placeholder="نام شخص ثالث را وارد کنید"
+                    maxLength={5}
+                    value={userData.personTwo}
+                />
+
+                <button
+                    type="submit"
+                    className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition mt-5"
+                >
+                    ارسال
+                </button>
+            </form>
+
+        </div>);
+}
+
+export default FormPage;
